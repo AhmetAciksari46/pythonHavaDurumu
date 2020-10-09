eb6d792e0b62b3fe7ccd5920ef4961af58dfc1ec
Ahmet Açıksarı <ahac46@gmail.com>
2020-10-10 00:22:23 +0300

    Add files via upload

---
+++ havadurumu.py
@@ -0,0 +1,20 @@
+#döviz kurları kodunu inceleyiniz!
+from selenium import webdriver  #sayfayı açmak için
+import time  #assayfa yüklenmesi sırasında bekleme yapmak için
+from bs4 import BeautifulSoup   #sayfa içerisinde ki sadece ilgili kısımları bulup almak için
+browser=webdriver.Chrome()  #chrome aracılığılya ilgili siteye bağlanılacağı belirtildi.
+browser.get("https://tr.freemeteo.com/havadurumu/iskenderun/7-days/list/?gid=311111&language=turkish&country=turkey")
+html = browser.page_source      
+time.sleep(2)
+soup = BeautifulSoup(html, 'html.parser')
+ilkParca=soup.find("div",{"class": "today sevendays"})   #sayfada bulmasını isteiğimiz kısım.sayfada 1 tane olduğu için find_all değilde find yazıldı.
+bilgiler=ilkParca.find_all("div",{"class": "hover"})
+
+for bilgi in bilgiler:   
+    tarih=bilgi.find_all("div",{"class": "title"})
+    tarihler = ((str(tarih).split("<span>",2)[1]).split("<",1)[0])
+    asgariDerece = bilgi.find_all("div",{"class": "temps"})
+    derece=((str(asgariDerece).split("<span>",1))[1].split("<",1))[0]
+    print(tarihler+"="+derece)
+    
+browser.close()
