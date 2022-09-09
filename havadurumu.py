6a79cdc267f43be82c39e476e69606e406ac1470
teasolves <bkaraaslan@gmail.com>
2022-09-09 19:56:35 +0300

    Update havadurumu.py

--- havadurumu.py
+++ havadurumu.py
@@ -1,4 +1,4 @@
-#döviz kurları kodunu incele
+#döviz kurları kodunu incele deneme
 from selenium import webdriver  #sayfayı açmak için
 import time  #assayfa yüklenmesi sırasında bekleme yapmak için
 from bs4 import BeautifulSoup   #sayfa içerisinde ki sadece ilgili kısımları bulup almak için
